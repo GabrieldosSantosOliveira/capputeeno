18d89a958bf422236bf0174f3448d197ce2ae181
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-06-22 22:08:12 -0400

    chore: add config eslint @rocketseat/eslint-config

--- capputeeno/src/pages/_document.tsx
+++ capputeeno/src/pages/_document.tsx
@@ -1,8 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Document, { DocumentContext } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument  extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
@@ -23,16 +29,16 @@ export default class MyDocument  extends Document {
       sheet.seal()
     }
   }
+
   render() {
     return (
       <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     )
-    
-    }
+  }
 }
